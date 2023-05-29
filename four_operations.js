62196271368f13b9931566754ad2eeb6428b7cbf
patshikwe <patshikwe@yahoo.fr>
2023-05-29 18:12:14 +0200

    4 basic math operations

---
+++ four_operations.js
@@ -0,0 +1,54 @@
+/** 4 basic math operations **/
+
+// Addition
+// const resultAdd = 2 + 2
+// console.log("resultAdd =>", resultAdd)
+// console.log("resultAdd is", typeof(resultAdd))
+
+// const resultAddString = 2 + "2"
+// console.log("resultAddString =>", resultAddString)
+// console.log("resultAddString is", typeof(resultAddString))
+/* 
+English : adding a number and a character string 
+causes a merger of these 2 elements = CONCATENATION
+
+french : l'addition d'un chiffre et d'une chaîne de caractère 
+provoque une fusion de ces 2 éléments = CONCATENATION
+*/
+
+/* ================================== */
+
+// Substraction
+// const resultSub = 4 - 2
+// console.log("resultSub =>", resultSub)
+// console.log("resultSub is", typeof(resultSub))
+
+// const resultSubNumberString = 4 - "2"
+// console.log("resultSubNumberString =>", resultSubNumberString)
+// console.log("resultSubNumberString is", typeof(resultSubNumberString))
+
+/* ================================== */
+
+// Multiplication
+// const resultMulti = 5 * 3
+// console.log("resultMulti =>", resultMulti)
+// console.log("resultMulti is", typeof(resultMulti))
+
+// const resultMultiNumberString = 5 * "3"
+// console.log("resultMultiNumberString =>", resultMultiNumberString)
+// console.log("resultMultiNumberString is", typeof(resultMultiNumberString))
+
+/* ================================== */
+
+// Division
+// const resultDivision = 20 / 4
+// console.log("resultDivision =>", resultDivision)
+// console.log("resultDivision is", typeof(resultDivision))
+
+// const resultDivisNumberString = 20 / "4"
+// console.log("resultDivisNumberString =>", resultDivisNumberString)
+// console.log("resultDivisNumberString is", typeof(resultDivisNumberString))
+
+
+
+
